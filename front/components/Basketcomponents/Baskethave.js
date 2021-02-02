43ac2f66966f071feaf6690ce8ec868de1dd258f
lss8314 <lss8314@naver.com>
2021-02-03 01:55:18 +0900

    장바구니폼 마무리

--- front/components/Basketcomponents/Baskethave.js
+++ front/components/Basketcomponents/Baskethave.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Row, Col,  Checkbox, Divider } from "antd";
+import {Row, Col,  Checkbox, Divider, Button } from "antd";
 // import TGLPick from "../IndexComponents/TglPick";
 import imgfile1 from '../../img/book1.png';
 import imgfile2 from '../../img/book2.png';
@@ -21,7 +21,7 @@ const dummylist1 = {
 
 }
 const dummylist2 = {
-    id : 1,
+    id : 2,
     product : "인생은 이태현처럼",
     option :"RED", number :"2",
     point :"999P",
@@ -31,13 +31,21 @@ const dummylist2 = {
 
 }
 const dummylist3 = {
-    id : 1,
+    id : 3,
     product : "김치는 한국거다",
     option :"RED", number :"2",
     point :"999P",
     delivery : "기본 배송",
     price :"200,00,000",
     ToTal : "200,00,000",
+
+}
+
+const Total = {
+   Totalproduct : " 100,000 ",
+    Totaldelivery : "4,000",
+    Totaldelmoney : "50,000",
+
 
 }
 
@@ -64,8 +72,8 @@ const Baskethave =()=>{
                     <Col md = {3}> </Col>
                     <Col md = {3} style={bb4}> 적립포인트 </Col>
                     <Col md = {2} style={bb5}> 배송구분 </Col>
-                    <Col md = {2} style={bb6}> 배송비 </Col>
-                    <Col md = {3} style={bb7}> TOTAL </Col>
+                    <Col md = {3} style={bb6}> 배송비 </Col>
+                    <Col md = {1} style={bb7}> TOTAL </Col>
                 </Row>
                 <Row style = {bb00}>
                     <Col md = {2}  style={bb0}><Checkbox onChange={onChange}></Checkbox> </Col>
@@ -76,8 +84,8 @@ const Baskethave =()=>{
                     <Col md = {3}> </Col>
                     <Col md = {3}   style={bb4}> {dummylist1.point}  </Col>
                     <Col md = {2}  style={bb5}> {dummylist1.delivery}  </Col>
-                    <Col md = {2}  style={bb6}> {dummylist1.price}  </Col>
-                    <Col md = {3}  style={bb7}> {dummylist1.ToTal}  </Col>
+                    <Col md = {3}  style={bb6}> {dummylist1.price}  </Col>
+                    <Col md = {1}  style={bb7}> {dummylist1.ToTal}  </Col>
                 </Row>
                 <Row style = {bb00}>
                     <Col md = {2}  style={bb0}><Checkbox onChange={onChange}></Checkbox> </Col>
@@ -88,8 +96,8 @@ const Baskethave =()=>{
                     <Col md = {3}> </Col>
                     <Col md = {3}   style={bb4}> {dummylist2.point}  </Col>
                     <Col md = {2}  style={bb5}> {dummylist2.delivery}  </Col>
-                    <Col md = {2}  style={bb6}> {dummylist2.price}  </Col>
-                    <Col md = {3}  style={bb7}> {dummylist2.ToTal}  </Col>
+                    <Col md = {3}  style={bb6}> {dummylist2.price}  </Col>
+                    <Col md = {1}  style={bb7}> {dummylist2.ToTal}  </Col>
                 </Row>
                 <Row style = {bb00}>
                     <Col md = {2}  style={bb0}><Checkbox onChange={onChange}></Checkbox> </Col>
@@ -100,10 +108,39 @@ const Baskethave =()=>{
                     <Col md = {3}> </Col>
                     <Col md = {3}   style={bb4}> {dummylist3.point}  </Col>
                     <Col md = {2}  style={bb5}> {dummylist3.delivery}  </Col>
-                    <Col md = {2}  style={bb6}> {dummylist3.price}  </Col>
-                    <Col md = {3}  style={bb7}> {dummylist3.ToTal}  </Col>
+                    <Col md = {3}  style={bb6}> {dummylist3.price}  </Col>
+                    <Col md = {1}  style={bb7}> {dummylist3.ToTal}  </Col>
                 </Row>
-                <Row style = {cc}> bb</Row>
+                <Row style = {cc}>
+                    <Col style ={cc0}><Button> 선택한 상품 삭제 </Button></Col>
+                    <Col style = {cc1}> 상품 구매 금액 : <b>{Total.Totalproduct}</b> + 배송비 {Total.Totaldelivery} = {Total.Totaldelmoney}원</Col>
+                </Row>
+            </div>
+            <div style={{marginTop:"15px"}}>
+                <Row style={dd}>
+                    <Col md ={5} style={dd0}> 총 상품 금액 </Col>
+                    <Col md ={1}></Col>
+                    <Col md ={3} style = {dd1}> 총 배송비</Col>
+                    <Col md ={9} style = {{marginLeft:"20px"}}></Col>
+                    <Col md ={4} style = {dd2} > 결제 예정 금액 </Col>
+                </Row>
+                <Row style = {ee}>
+                    <Col md ={5} style={dd0}> {Total.Totalproduct}원</Col>
+                    <Col md ={2} style = {{display: "flex", alignItems: "center"}}>+</Col>
+                    <Col md ={2} style={ee0}>{Total.Totaldelivery}원</Col>
+                    <Col md={4} style={ee1}>(40,000원 이상 무료배송)</Col>
+                    <Col md ={5}style={ee2}> =</Col>
+                    <Col md ={6}style={dd2}> {Total.Totaldelmoney}원</Col>
+                </Row>
+            </div>
+            <div style = {ff}>
+
+                <Col md={2}><Button> 쇼핑 계속하기</Button></Col>
+                <Col md={14}style ={{marginRight:"26px"}}></Col>
+                <Col md={4}><Button> 선택 상품 주문</Button></Col>
+                <Col md={2}><Button> 전체 상품 주문</Button></Col>
+
+
             </div>
         </>
     );
@@ -117,7 +154,7 @@ const aa ={
     height: "40px",
     fontSize : "17px",
     color: "gray",
-    marginBottom : "20px",
+    marginBottom : "30px",
 
 }
 const aa1 ={
@@ -172,10 +209,10 @@ const bb00 ={
 const bb0 ={
     width: "20px",
     display: "flex",
+    flexDirection:"row",
     justifyContent: "center",
     alignItems: "center",
- //   marginLeft : "10px",
- //   marginRight : "20px",
+    margin:"3px",
 }
 const bb1 ={
     display: "flex",
@@ -225,11 +262,92 @@ const bb7 ={
     alignItems: "center",
    // marginLeft: "40px",
 }
+
 const cc ={
+    display : "flex",
+    flexDirection:"row",
     backgroundColor : "lightgray",
     width :"900px",
-    height: "auto",
+    height: "50px",
+
+}
+const cc0 ={
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "10px",
+    marginLeft : "10px"
+
+}
+const cc1 ={
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "15px",
+    marginLeft: "412px",
+}
+const dd0 ={
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+}
+const dd1 ={
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "10px",
+}
+const dd2 ={
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+
 }
 
+const dd ={
+    display: "flex",
+    flexDirection:"row",
+    border: "1px solid lightgray",
+    width :"900px",
+    height: "40px",
+    fontSize : "14px",
+    color: "gray",
+
+}
+const ee ={
+    display: "flex",
+    flexDirection:"row",
+    border :"1px solid lightgray",
+    // justifyContent: "center",
+    // alignItems: "center",
+    width :"900px",
+    height: "50px",
+    fontSize : "20px",
+
+}
+
+const ee0 ={
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+
+}
+
+const ee1 ={
+    display: "flex",
+    alignItems:" center",
+    fontSize: "10px",
+}
+const ee2 ={
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+}
+
+const ff = {
+    marginTop :"15px",
+    display: "flex",
+    flexDirection:"row",
+    width :"900px",
+    height: "50px",
+}
 
 export default Baskethave;
